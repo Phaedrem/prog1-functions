8f5770eb9d7b82d35ce6b1e0f276125fa3ec230a
Darren Bowers <darren.bowers@g.austincc.edu>
2021-03-23 14:07:59 -0500

    Removed validation if statements from all functions

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@
 # SIZE is a global constant, use this throughout your program.
 # you should be able to change this to any integer > 0 and your
 # whole program will still work correctly. TEST THIS!
-SIZE = 10
+SIZE = 9
 MIN = 10
 MAX = 100
 
@@ -15,28 +15,19 @@ import numpy as np
 import random
 
 def make_array():
-    if isinstance(SIZE, int):
-        array = np.array([0] * SIZE, dtype=int)
-        for i in range(SIZE):
-            array[i] = random.randint(MIN,MAX)
-    else:
-        array = 0
+    array = np.array([0] * SIZE, dtype=int)
+    for i in range(SIZE):
+        array[i] = random.randint(MIN,MAX)
     return array
     
 def total(nums):
-    if all(nums > 0):
-        total_numbers = 0
-        for i in range(SIZE):
-            total_numbers += nums[i]
-    else:
-        total_numbers = 0
+    total_numbers = 0
+    for i in range(SIZE):
+        total_numbers += nums[i]
     return int(total_numbers)
 
 def average(nums):
-    if total(nums) > 0:
-        average_numbers = float(total(nums))/float(SIZE)
-    else:
-        average_numbers = 0
+    average_numbers = float(total(nums))/float(SIZE)
     return float(average_numbers)
 
 def minimum(nums):
